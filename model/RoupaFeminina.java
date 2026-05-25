d5f2a50d8b0903a4d08a3fb07d6e1d7df072dca2
Isaias Silva <isaias.silva.dev@gmail.com>
2026-05-25 16:29:42 -0300

    Update RoupaFeminina.java

--- model/RoupaFeminina.java
+++ model/RoupaFeminina.java
@@ -1,17 +1,25 @@
-package model; // Mesma pasta das outras roupas
+package model;
 
 public class RoupaFeminina extends Roupa {
-    private String categoriaFeminina; // Ex: Vestido, Saia, Calça, Blusa
+    private String categoriaFeminina;
 
+    // Construtores
     public RoupaFeminina() {
         super();
     }
 
     public RoupaFeminina(String nome, String marca, String tamanho, double preco, String cor, String categoriaFeminina) {
-        super(nome, marca, tamanho, preco, cor); // Envia os dados para a classe pai
+        super(nome, marca, tamanho, preco, cor);
         this.categoriaFeminina = categoriaFeminina;
     }
 
+    // Getter e Setter específico
     public String getCategoriaFeminina() { return categoriaFeminina; }
     public void setCategoriaFeminina(String categoriaFeminina) { this.categoriaFeminina = categoriaFeminina; }
+
+    // toString no final reaproveitando o super.toString()
+    @Override
+    public String toString() {
+        return super.toString() + " | Categoria: " + categoriaFeminina;
+    }
 }
